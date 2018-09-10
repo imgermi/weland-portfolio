77bdc3f7792c91ec88455479489add40cf7bd7fa
juangermano <juanmartingermano@gmail.com>
2018-09-10 20:23:37 -0300

    trabajos

--- pages/trabajos.vue
+++ pages/trabajos.vue
@@ -17,6 +17,42 @@
         <h2 class="hero__sub">Lo que pasa después de <br/> 2435,66 mails, varias reuniones <br/> y mucho feedback.</h2>
       </div>
     </section>
+    <section class="trabajos__listado">
+      <div class="container">
+        <div class="trabajos-container">
+          <nuxt-link to="" class="trabajos__listado--trabajo">
+            <img src="~/assets/images/case-studies/vitroblock/vitroblock-mockup.jpg" alt="">
+            <h3>Vitroblock</h3>
+            <span>Consultoría Google Ads</span>
+          </nuxt-link>
+          <nuxt-link to="" class="trabajos__listado--trabajo">
+            <img src="~/assets/images/case-studies/vendexa/vendexa-interiores-mockup.jpg" alt="">
+            <h3>Vendexa Interiores</h3>
+            <span>Tienda Online</span>
+          </nuxt-link>
+          <nuxt-link to="" class="trabajos__listado--trabajo">
+            <img src="~/assets/images/case-studies/fucer/fucer-mockup.jpg" alt="">
+            <h3>Fucer App</h3>
+            <span>Mobile App</span>
+          </nuxt-link>
+          <nuxt-link to="" class="trabajos__listado--trabajo">
+            <img src="~/assets/images/case-studies/standupweb/standupweb-mockup.jpg" alt="">
+            <h3>StandUpWeb</h3>
+            <span>Online Course</span>
+          </nuxt-link>
+          <nuxt-link to="" class="trabajos__listado--trabajo">
+            <img src="~/assets/images/case-studies/autopartgo/autopartgo-mockup.jpg" alt="">
+            <h3>AutoPartGo</h3>
+            <span>Sitio Web</span>
+          </nuxt-link>
+          <nuxt-link to="" class="trabajos__listado--trabajo">
+            <img src="~/assets/images/case-studies/panden/panden-mockup.jpg" alt="">
+            <h3>Panden Industrial</h3>
+            <span>Sitio Web</span>
+          </nuxt-link>          
+        </div>
+      </div>
+    </section>
     <Contacto/>
   </main>
 </template>
